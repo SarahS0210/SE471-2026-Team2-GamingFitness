49977626bf261293e40f276a5290a5885cab987e
Judah-F <Judahfisher21@gmail.com>
2025-03-11 09:44:50 -0700

    Update and rename UserAccount.java to User.java

--- BackEndJava/User.java
+++ BackEndJava/User.java
@@ -1,16 +1,3 @@
-public class Main {
-  public static void main(String[] args) {
-    User player = new User();
-    player.change_email();
-    player.change_password();
-    player.change_username();
-    player.check_progress();
-    System.out.println(player.getEmail());
-    System.out.println(player.getPassword());
-    System.out.println(player.getEmail());
-  }
-
-
 import java.util.Scanner;
 
 
