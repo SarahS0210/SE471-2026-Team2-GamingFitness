b396a16a276b316570e61a1839f54af549652c55
Judah-F <Judahfisher21@gmail.com>
2025-04-26 13:54:08 -0700

    Update UserRepository.java

--- src/main/java/com/example/servingwebcontent/UserRepository.java
+++ src/main/java/com/example/servingwebcontent/UserRepository.java
@@ -3,10 +3,15 @@ package com.example.servingwebcontent;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+/*
+ * This class is our repository it is a REST API which allows our front end to interact with our User class
+ */
+@RepositoryRestResource(collectionResourceRel = "users", path = "app_users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long>, CrudRepository<User,Long> {
 
-public interface UserRepository extends CrudRepository<User, Long> {
+  List<User> findByUserName(@Param("userName") String userName);
 
-  List<User> findByLastName(String lastName);
-
-  User findById(long id);
 }
