6ae2bff56f1b125edea6b0f74610dbb610bbbc8c
Nikita Sharma <sharm046@csusm.edu>
2026-04-27 12:30:18 -0700

    Delete src/main/java/com/example/servingwebcontent/UserBuilder.java

--- src/main/java/com/example/servingwebcontent/UserBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.example.servingwebcontent;
-
-public interface UserBuilder {
-    UserBuilder setUserName(String userName);
-    UserBuilder setPassword(String password);
-    UserBuilder setStyle(String style);
-    UserBuilder setShots(int shots);
-    User build();
-}
